81442a324a23494c149b536b86e66a5a8e45571c
Alibert <aliberth33@gmail.com>
2024-11-08 22:47:49 -0400

    devoleping guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
-use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("guess the number!");
     println!("");
     println!("Please input your guess");
 
-    let mut guess = String::new();
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    // let mut guess: String = String::new();
+    let mut guess: String = String::new();
+    let secret_number: i32 = rand::thread_rng().gen_range(1..=100);
 
     io::stdin()
         .read_line(&mut guess)
@@ -17,4 +19,13 @@ fn main() {
     println!("Your guessed: {}", guess);
     println!("");
     println!("Your guessed: {secret_number}");
+
+    let guess_number: i32 = guess.trim().parse().unwrap();
+
+    println!("");
+    match guess_number.cmp(&secret_number) {
+        Ordering::Less => println!("you guess is Smaller!"),
+        Ordering::Greater => println!("you guess is Bigger!"),
+        Ordering::Equal => println!("Yeah baby!! match!!!"),
+    }
 }
