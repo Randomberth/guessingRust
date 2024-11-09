77fb3896579319ef46967dd13cdd5de5fc0c9535
Alibert <aliberth33@gmail.com>
2024-11-08 23:12:19 -0400

    devoleping guessing game (1)

--- src/main.rs
+++ src/main.rs
@@ -5,27 +5,30 @@ use std::io;
 fn main() {
     println!("guess the number!");
     println!("");
-    println!("Please input your guess");
 
-    // let mut guess: String = String::new();
-    let mut guess: String = String::new();
-    let secret_number: i32 = rand::thread_rng().gen_range(1..=100);
+    loop {
+        println!("Please input your guess");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        // let mut guess: String = String::new();
+        let mut guess: String = String::new();
+        let secret_number: u32 = rand::thread_rng().gen_range(1..=100);
 
-    println!("");
-    println!("Your guessed: {}", guess);
-    println!("");
-    println!("Your guessed: {secret_number}");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let guess_number: i32 = guess.trim().parse().unwrap();
+        println!("");
+        println!("Your guessed: {}", guess);
+        println!("");
+        println!("Generated: {secret_number}");
 
-    println!("");
-    match guess_number.cmp(&secret_number) {
-        Ordering::Less => println!("you guess is Smaller!"),
-        Ordering::Greater => println!("you guess is Bigger!"),
-        Ordering::Equal => println!("Yeah baby!! match!!!"),
+        let guess_number: u32 = guess.trim().parse().expect("Please type a number!");
+
+        println!("");
+        match guess_number.cmp(&secret_number) {
+            Ordering::Less => println!("you guess is Smaller!"),
+            Ordering::Greater => println!("you guess is Bigger!"),
+            Ordering::Equal => println!("Yeah baby!! match!!!"),
+        }
     }
 }
